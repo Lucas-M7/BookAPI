10abd855cb1bc8343892394aa83f5ec2047e321b
Lucas Mateus <Lucas015x@gmail.com>
2024-04-18 19:02:22 -0300

    Apagando código no DBContext

--- Infraestruture/DB/DBConnectContext.cs
+++ Infraestruture/DB/DBConnectContext.cs
@@ -4,20 +4,6 @@ using Microsoft.EntityFrameworkCore;
 namespace BookAPI.Infraestruture.DB;
 public class DBConnectContext(DbContextOptions<DBConnectContext> options) : DbContext(options)
 {
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<User>().HasData(
-            new User
-            {
-                Id = 1,
-                Name = "Lucas",
-                Email = "Lucas@teste.com",
-                Password = "pass123",
-                Profile = "ADM"
-            }
-        );
-    }
-
     public DbSet<User> Users { get; set; } = default!;
     public DbSet<Book> Books { get; set; } = default!;
 }
